0d82ce34ccd7bd5153720762d767933183dacdd6
geekuz <otabekgeek@gmail.com>
2025-02-27 18:00:22 +0500

    Kadanes_Algorithm

---
+++ src/leetcode1/Kadanes_Algorithm.java
@@ -0,0 +1,14 @@
+package leetcode1;
+
+public class Kadanes_Algorithm {
+    public int maxSubarraySum(int[] nums) {
+        int maxSum = nums[0], maxSumEndingHere = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            maxSumEndingHere = Math.max(nums[i], maxSumEndingHere + nums[i]);
+            maxSum = Math.max(maxSum, maxSumEndingHere);
+        }
+
+        return maxSum;
+    }
+}
